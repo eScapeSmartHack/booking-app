4c2cc02d97b1b1cfa06a83a410e9e855ac1261b7
popescurebeca <popescurebeca_wis@yahoo.com>
2025-11-08 16:50:06 +0200

    test routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 from app.api.routes import api_router
+from pydantic import BaseModel
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
@@ -19,6 +20,17 @@ app.add_middleware(
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
+class Room(BaseModel):
+    room_id: int
+    data: str
+    
+
+class Booking(BaseModel):
+    booking_id: int
+    room_id: int
+    start: str
+    end: str
+
 @app.get("/")
 async def root():
     return {"message": "Welcome to the API"}
@@ -26,3 +38,27 @@ async def root():
 @app.get("/health")
 async def health_check():
     return {"status": "healthy"}
+
+
+rooms = [
+		Room(room_id=1, data="json ceva laalla"),
+		Room(room_id=2, data="json ceva lalala"),
+	]	
+
+bookings = []
+
+@app.get("/rooms")
+async def get_rooms():
+    # This is a placeholder for actual room retrieval logic
+    # trebuie luate din baza de date camerele
+	return rooms
+
+@app.post("/bookings")
+async def create_booking(booking: Booking):
+    booking_dict = booking.model_dump()
+    bookings.append(booking_dict)
+    return {"message": "Booking created", "booking": booking_dict}
+
+@app.get("/bookings")
+async def get_bookings():
+     return {"message": "List of bookings", "bookings": bookings}
